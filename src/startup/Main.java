24bcc3cb8a0a580d544550f4199292379ba7b650
Skinken123 <guma.moritz@gmail.com>
2024-04-30 15:59:34 +0200

    first commit

---
+++ src/startup/Main.java
@@ -0,0 +1,16 @@
+package startup;
+
+/**
+ * Contains the <code>main</code> method. Performs all startup of the application.
+ */
+
+public class Main { 
+    /**
+     * Starts the application.
+     * 
+     * @param args The application does not take any command line parameters.
+     */
+    public static void main(String[] args) {
+        System.out.println("Starting the application.");
+    }
+}
